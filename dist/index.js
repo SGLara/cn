5ba4077f73cb3f7aa5efad7853d09c6006718815
Steven Lara <steven.gustavo.lara@gmail.com>
2025-01-13 19:33:34 -0600

    fix: types

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cn = void 0;
-const clsx_1 = require("clsx");
-const tailwind_merge_1 = require("tailwind-merge");
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 /**
  * Combines clsx and tailwind-merge for optimal class merging.
  * @param inputs - Class names or conditional class values.
  * @returns A single string with merged class names.
  */
-const cn = (...inputs) => {
-    return (0, tailwind_merge_1.twMerge)((0, clsx_1.clsx)(inputs));
+export const cn = (...inputs) => {
+    return twMerge(clsx(inputs));
 };
-exports.cn = cn;
