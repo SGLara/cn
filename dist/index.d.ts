ade1fb6e4b22b79380e869bba95856dd533ad3eb
Steven Gustavo Lara <58200255+SGLara@users.noreply.github.com>
2025-05-22 00:06:10 -0600

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { ClassValue } from "clsx";
-/**
- * Combines clsx and tailwind-merge for optimal class merging.
- * @param inputs - Class names or conditional class values.
- * @returns A single string with merged class names.
- */
-export declare const cn: (...inputs: ClassValue[]) => string;
